5550d77ef82b8267a6123d805972a9eec3d1a903
Tony Ruiz <luisantruiz@gmail.com>
2016-10-02 12:49:03 -0600

    Main functionality, Static class, Unit Test, nuspec file

---
+++ ExceptionMsgConcatener.Tests/Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ExceptionMsgConcatener.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void ShouldRunExceptionToException()
+        {
+            var innerException = new Exception("There was another error");
+            var exception = new Exception("There was an error", innerException);
+
+            var newException = ExceptionMsgConcatener.ExceptionToException(exception);
+            Assert.AreEqual(newException.Message, $"{exception.Message}, {innerException.Message}");
+        }
+
+        [TestMethod]
+        public void ShouldRunExceptionToString()
+        {
+            var innerException = new Exception("There was another error");
+            var exception = new Exception("There was an error", innerException);
+
+            var newExceptionMessage = ExceptionMsgConcatener.ExceptionToString(exception);
+            Assert.AreEqual(newExceptionMessage, $"{exception.Message}, {innerException.Message}");
+        }
+    }
+}
---
+++ ExceptionMsgConcatener/ExceptionMsgConcatener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExceptionMsgConcatener
+{
+    public static class ExceptionMsgConcatener
+    {
+        public static string ExceptionToString(Exception exception)
+        {
+            var newException = ExceptionToException(exception);
+
+            return GetExceptionMessage(newException);
+        }
+
+        public static Exception ExceptionToException(Exception exception)
+        {
+            var messages = GetExceptionMessage(exception);
+
+            var baseException = exception;
+            while (baseException.InnerException != null)
+            {
+                baseException = baseException.InnerException;
+                messages += $", {GetExceptionMessage(baseException)}";
+            }
+
+            exception = new Exception(messages);
+
+            return exception;
+        }
+
+        private static string GetExceptionMessage(Exception exception)
+        {
+            return exception.Message;
+        }
+    }
+}
